dc234751714ccfc30a60c3497da51c565aee9aed
kailashmishra12 <140958362+kailashmishra12@users.noreply.github.com>
2023-07-30 22:29:03 +0530

    Add files via upload

---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <br></br>
+            <hr></hr>
+            <p className="footer-text">Made with ♥️ by Rakshita<br></br>
+            <br></br>
+            <small>All rights reserved © 2023 Rakshita Gowda</small><br></br>
+            <small>Illustrations by undraw.co</small>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
